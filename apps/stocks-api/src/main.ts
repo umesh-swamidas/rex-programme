04e01c864db2d0296e55a5ef295c0c15162e47c3
uswamidas <umesh.swamidas2@t-mobile.com>
2020-01-16 12:15:26 -0800

    Initial commit

--- apps/stocks-api/src/main.ts
+++ apps/stocks-api/src/main.ts
@@ -10,13 +10,41 @@ const init = async () => {
     host: 'localhost'
   });
 
+  const httpRequest = require('request');
+
   server.route({
     method: 'GET',
     path: '/',
-    handler: (request, h) => {
-      return {
-        hello: 'world'
-      };
+    handler: (request, reply) => {
+      console.log('request.params: ' + request.params);
+      console.log('request.path: ' + request.path);
+      console.log('request.query: ' + Object.prototype.hasOwnProperty.call(request.query, 'token'));
+      /*return {
+        hello: 'umesh'
+      };*/
+
+      let httpResponse: any = {};
+      httpRequest('https://sandbox.iexapis.com/stable/stock/AAPL/chart/1m?token=Tpk_cb788a12066c4676b4a6f74dac0ba44c', (error, response, body) => {
+        console.error('error:', error);
+        console.log('statusCode:', response && response.statusCode);
+        console.log('body:', body);
+        httpResponse = body;
+        //reply.response(body);
+        //console.log('HTTP Response:', reply);
+        //return reply;
+        /*httpResponse = {
+          hello: 'umesh'
+        };*/
+      });
+
+    /* httpRequest('https://sandbox.iexapis.com/stable/stock/AAPL/chart/1m?token=Tpk_cb788a12066c4676b4a6f74dac0ba44c')
+        .on('response', function(response) {
+          console.log(response.statusCode)
+          console.log(response.headers['content-type'])
+          httpResponse = response.body;
+        });*/
+      //console.log('HTTP Response:', reply);
+      return httpResponse;
     }
   });
 
